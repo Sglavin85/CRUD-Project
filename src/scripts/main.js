8d34c98ea31cd7740f9ba612eb1535d5cb129998
Sean Glavin <sean@Seans-MacBook-Pro.local>
2019-05-15 13:22:02 -0500

    add interest complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 
-import APIManager from "./dbCalls";
+import API from "./dbCalls";
 import postAllInterests from "./interestsToDom"
+import newInterestListener from "./AddInterestModal"
+
+// fill the drop down menu
+API.getAllPlaces()
+    .then(places => {
+        let dropdownMenu = document.querySelector("#interestLocation")
+        places.forEach(place => {
+            let frag = document.createDocumentFragment()
+            let option = document.createElement("option")
+            option.setAttribute("value", `${place.id}`)
+            option.textContent = place.name
+            frag.appendChild(option)
+            dropdownMenu.appendChild(frag)
+        })
+
+    })
+
 
 postAllInterests();
+newInterestListener();
+
